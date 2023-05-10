5726a4b8f848370408e490b872474ca37afaeb90
Dylan Wishner <dylan.wishner@gmail.com>
2023-05-10 16:02:27 -0700

    Rename owgym to offworld_gym

--- scripts/launch_offworld_gym_tracking.sh
+++ scripts/launch_offworld_gym_tracking.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Service manager currently does not support ros2 launch files, so we use this script as an OS_SERVICE to launch the node
-ros2 launch apriltag_ros owgym_env_tracking.py
+ros2 launch apriltag_ros offworld_gym_env_tracking.py
